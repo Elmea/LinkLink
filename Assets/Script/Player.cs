d196c3635dd423a7803603aa00bd6aa980dede60
Masseteau Emma <n.masseteau@student.isartdigital.com>
2023-09-05 11:45:19 +0200

    Add player physic sim

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,13 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
 
 public class Player : MonoBehaviour
 {
+    private Vector3 velocity;
+    private Rigidbody2D body;
+    [SerializeField] private bool offWall = false;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        body = GetComponent<Rigidbody2D>();
+    }
+    
+    private void FixedUpdate()
+    {
+        if (offWall)
+        {
+            velocity += WorldSettings.gravity * Time.fixedDeltaTime;
+            transform.position += velocity * Time.fixedDeltaTime;
+        }
     }
 
     // Update is called once per frame
